2dac62daf5efd247d09cfe5dd4b350ea939069d4
Dima <117075948+Dmitri28@users.noreply.github.com>
2022-11-01 15:36:52 +0200

    Replay2
    Replay2

---
+++ Replay2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Replay2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name;
+            Console.WriteLine("Enter your name");
+            name = Console.ReadLine();
+            if(name!=" ")
+            {
+                Console.WriteLine("Helow " + name);
+            }
+            else
+            {
+                Console.WriteLine("Hellow");
+            }
+        }
+    }
+}
